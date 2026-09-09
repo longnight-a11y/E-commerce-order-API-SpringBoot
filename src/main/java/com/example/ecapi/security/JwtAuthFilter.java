c9145a18252d285ec2f3a179d01e563c5dd9e3af
Mirai <LONG_NOGHT@icloud.com>
2026-09-09 20:51:43 +0900

    wip: Add JwtAuthFilter (update after implementing entities and repositories)

---
+++ src/main/java/com/example/ecapi/security/JwtAuthFilter.java
@@ -0,0 +1,17 @@
+package com.example.ecapi.security;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+//@Component
+//public class JwtAuthFilter extends OncePerRequestFilter {
+//
+//    private JwtService jwtService;
+//    private
+//
+//    @Override
+//    protected void doFilterInternal(){
+//        //
+//    }
+//
+//}
