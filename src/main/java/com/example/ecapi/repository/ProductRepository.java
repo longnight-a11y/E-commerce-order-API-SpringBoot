e19817747ae54ab5dc3d13d650ecc0a66701eb70
Mirai <LONG_NOGHT@icloud.com>
2026-09-13 19:22:53 +0900

    feat: Add seller to Product entity and ProductRepository

--- src/main/java/com/example/ecapi/repository/ProductRepository.java
+++ src/main/java/com/example/ecapi/repository/ProductRepository.java
@@ -8,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-    Page<Product> findByUserId(UUID userId, Pageable pageable);
+    Page<Product> findBySellerId(UUID sellerId, Pageable pageable);
+    Page<Product> findByCategoryId(UUID categoryId, Pageable pageable);
 }
