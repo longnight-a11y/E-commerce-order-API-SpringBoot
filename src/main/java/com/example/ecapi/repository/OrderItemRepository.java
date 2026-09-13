a9d0ffb6e6b1c5b65df0c19aeba0544c466c3a3f
Mirai <LONG_NOGHT@icloud.com>
2026-09-13 18:51:18 +0900

    feat: Add repositories for user, product, cart, and order

---
+++ src/main/java/com/example/ecapi/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.ecapi.repository;
+
+import com.example.ecapi.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
+}
