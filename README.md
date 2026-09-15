42024cb023466a193886bb00a74c06eb4a0a1987
Mirai <LONG_NOGHT@icloud.com>
2026-09-15 22:31:26 +0900

    feat: Add JwtAuthFilter

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 ![CI](https://github.com/longnight-a11y/E-commerce-order-API-SpringBoot/actions/workflows/ci.yml/badge.svg)
 
 # E-commerce Order API
+
+## Spring Security
+
+```
+Client
+  │
+  │ Authorization: Bearer <JWT>
+  ↓
+Spring Security
+  │
+  ├─ BearerTokenAuthenticationFilter
+  │
+  ├─ JwtDecoder
+  │
+  ├─ JwtAuthenticationConverter
+  │       ↓
+  │    ROLE_CUSTOMER
+  │    ROLE_SELLER
+  │    ROLE_ADMIN
+  │
+  ↓
+SecurityContextHolder
+  │
+  ↓
+Authorization
+  │
+  ├─ CUSTOMER
+  ├─ SELLER
+  └─ ADMIN
+  │
+  ↓
+Controller
+  ↓
+Service
+```
--- src/main/java/com/example/ecapi/security/JwtAuthFilter.java
+++ src/main/java/com/example/ecapi/security/JwtAuthFilter.java
@@ -1,17 +1,47 @@
 package com.example.ecapi.security;
 
+import com.example.ecapi.repository.UserRepository;
+import io.jsonwebtoken.Jwt;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-//@Component
-//public class JwtAuthFilter extends OncePerRequestFilter {
-//
-//    private JwtService jwtService;
-//    private
-//
-//    @Override
-//    protected void doFilterInternal(){
-//        //
-//    }
-//
-//}
+import java.io.IOException;
+import java.util.Collections;
+import java.util.UUID;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private JwtService jwtService;
+    private UserRepository userRepository;
+
+    public JwtAuthFilter(JwtService jwtService, UserRepository userRepository){
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if(header != null && header.startsWith("Bearer ")){
+            String token = header.substring(7);
+            UUID userId = jwtService.decodeToken(token);
+            if(userId != null){
+                userRepository.findById(userId).ifPresent(user -> {
+                    var auth = new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                });
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
